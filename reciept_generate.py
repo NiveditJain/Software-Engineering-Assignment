52895c996075b0e02ca0ebaaea628820daed56fe
NiveditJain <jain.22@iitj.ac.in>
2020-06-22 00:34:27 +0530

    written 10 test cases, all passed

---
+++ reciept_generate.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from PIL import Image, ImageDraw, ImageFont
+
+# Class to generate Reciept
+class GenerateReciept():
+    
+    # constructor for Class
+    def __init__(self,reciept_number):
+        self.reciept_number=reciept_number
+        self.request_link="https://mathrithms.github.io/reciept/"+reciept_number+".json"
+        self.response=json.loads(requests.get(self.request_link).text)
+
+    def export(self):
+
+        # making imahe and text color settings
+        im=Image.open('template.jpg')
+        d=ImageDraw.Draw(im)
+        text_color = (0, 0, 0)
+
+        # writing reciept number
+        font = ImageFont.truetype("C:\\Windows\\Fonts\\Arial\\arialbd.ttf", 40)
+        d.text((537, 121), self.response["reciept_number"], fill = text_color, font = font)
+
+        # writing all other details
+        font = ImageFont.truetype("C:\\Windows\\Fonts\\Arial\\arialbd.ttf", 30)
+        d.text((154, 163), self.response["name"], fill = text_color, font = font)
+        d.text((286, 233), self.response["designation"], fill = text_color, font = font)
+        d.text((94, 304), self.response["id"], fill = text_color, font = font)
+        d.text((187, 375), self.response["status"], fill = text_color, font = font)
+        d.text((416, 443), self.response["requested"], fill = text_color, font = font)
+        d.text((290, 512), self.response["paid"], fill = text_color, font = font)
+        d.text((312, 579), self.response["balance left"], fill = text_color, font = font)
+        d.text((27, 756), self.response["signatures"], fill = text_color, font = font)
+        d.text((640, 441), self.response["currency"], fill = text_color, font = font)
+        d.text((640, 507), self.response["currency"], fill = text_color, font = font)
+        d.text((640, 575), self.response["currency"], fill = text_color, font = font)
+
+        im.save("reciept_"+self.response["reciept_number"]+".pdf")
---
+++ test.py
@@ -0,0 +1,110 @@
+from reciept_generate import GenerateReciept
+import unittest
+import os
+
+class TestGenerateReciept(unittest.TestCase):
+
+
+    # checking reciept number must be same in json 
+    # object fetched and requested number
+    def test_reciept_number(self):
+        reciept=GenerateReciept("10010010")
+        self.assertEqual(reciept.response["reciept_number"],"10010010")
+
+
+
+    # testing that no json object is empty
+    def test_no_empty(self):
+        reciept=GenerateReciept("10010010")
+        self.assertNotEqual("",reciept.response["reciept_number"])
+        self.assertNotEqual("",reciept.response["name"])
+        self.assertNotEqual("",reciept.response["designation"])
+        self.assertNotEqual("",reciept.response["id"])
+        self.assertNotEqual("",reciept.response["status"])
+        self.assertNotEqual("",reciept.response["requested"])
+        self.assertNotEqual("",reciept.response["paid"])
+        self.assertNotEqual("",reciept.response["currency"])
+        self.assertNotEqual("",reciept.response["balance left"])
+        self.assertNotEqual("",reciept.response["signatures"])
+
+
+
+    # check that designation must among some
+    def test_designation(self):
+        reciept=GenerateReciept("10010010")
+        allowed_designations=["Student","Professor","Assistant Professor",
+        "Associate Professor","Director","Associate Dean","Dean"
+        ]
+        self.assertIn(reciept.response["designation"],allowed_designations)
+
+
+
+    # test if file is successfully generated
+    def test_reciept_generation(self):
+        reciept=GenerateReciept("10010010")
+
+        if os.path.exists("reciept_10010010.pdf"):
+            os.remove("reciept_10010010.pdf")
+
+        reciept.export()        
+        self.assertIn("reciept_10010010.pdf",os.listdir())
+
+        if os.path.exists("reciept_10010010.pdf"):
+            os.remove("reciept_10010010.pdf")
+
+
+
+    # status must be among some
+    def test_status(self):
+        reciept=GenerateReciept("10010010")
+        allowed_status=["Approved","Requested","Settled"]
+        self.assertIn(reciept.response["status"],allowed_status)
+
+
+
+    # requested must be less than 1.5 core
+    def test_requested(self):
+        reciept=GenerateReciept("10010010")
+        self.assertGreaterEqual(int(reciept.response["requested"]),0)
+        self.assertLessEqual(int(reciept.response["requested"]),15000000)
+
+
+
+    #paid must be less than equal 90% of requested
+    def test_paid(self):
+        reciept=GenerateReciept("10010010")
+        self.assertLessEqual(int(reciept.response["paid"]),0.9*int(reciept.response["requested"]))
+
+           
+
+    # only some currencies are allowed
+    def  test_currency(self):
+        reciept=GenerateReciept("10010010")
+        allowed_currcies=["INR","USD"]
+        self.assertIn(reciept.response["currency"],allowed_currcies)
+
+
+
+    # balance left
+    def test_balance_left(self):
+        reciept=GenerateReciept("10010010")
+
+        if(reciept.response["status"]!="Settled"):
+            self.assertGreaterEqual(int(reciept.response["balance left"]),0.9*int(reciept.response["requested"]))
+        else:
+            self.assertGreaterEqual(int(reciept.response["balance left"]),0)
+
+
+
+
+    # only few people can sign
+    def test_signatures(self):
+        reciept=GenerateReciept("10010010")
+        allowed_signatures=["Director","Dean"]
+        self.assertIn(reciept.response["signatures"],allowed_signatures)
+
+
+
+# test case runner
+if __name__ == '__main__':
+    unittest.main()
